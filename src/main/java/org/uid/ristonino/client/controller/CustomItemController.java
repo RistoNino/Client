d56486e49554aed0929c0b045a74618b87805e7d
Shadw <34161328+Shadowz-git@users.noreply.github.com>
2024-05-22 16:21:28 +0200

    Creazione del file popup per la creazione di un Item custom con ingredienti custom

--- src/main/java/org/uid/ristonino/client/controller/CustomItemController.java
+++ src/main/java/org/uid/ristonino/client/controller/CustomItemController.java
@@ -1,4 +1,30 @@
 package org.uid.ristonino.client.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.util.List;
+
 public class CustomItemController {
+    @FXML
+    private VBox ingredientsBox;
+    @FXML
+    private VBox extraIngredientsBox;
+
+    private List<String> ingredients;
+
+    public void initialize(List<String> ingredients) {
+        this.ingredients = ingredients;
+        for (String ingredient : ingredients) {
+            CheckBox checkBox = new CheckBox(ingredient);
+            ingredientsBox.getChildren().add(checkBox);
+        }
+//        for (String extraIng : extraIngredients) {
+//
+//        }
+    }
+
 }
