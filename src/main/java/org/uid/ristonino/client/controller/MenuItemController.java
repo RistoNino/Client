71e920a929430aac11020442e1b822ae59e0013b
Shadw <34161328+Shadowz-git@users.noreply.github.com>
2024-05-22 16:26:49 +0200

    Aggiornata la gestione di quantità e design degli Item

--- src/main/java/org/uid/ristonino/client/controller/MenuItemController.java
+++ src/main/java/org/uid/ristonino/client/controller/MenuItemController.java
@@ -5,14 +5,20 @@ import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.CustomMenuItem;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.scene.transform.Scale;
 import javafx.scene.transform.Translate;
 import javafx.util.Duration;
+import org.kordamp.ikonli.javafx.FontIcon;
+import org.uid.ristonino.client.model.Settings;
 
+import java.io.IOException;
 import java.util.List;
 
 public class MenuItemController {
@@ -21,13 +27,18 @@ public class MenuItemController {
     @FXML private Button remove;
     @FXML private Label quantity;
     @FXML private Label price;
-    @FXML private Label total;
     @FXML private Label name;
     @FXML private Label description;
     @FXML private Label ingredients;
     @FXML private HBox itemView;
 
+
+
     private int itemQuantity = 0;
+
+    private FontIcon addIcon = new FontIcon("mdoal-add");
+    private FontIcon removeIcon = new FontIcon("mdomz-minus");
+
     private KeyValue keyValueCenter;
     private Timeline timelineCenter;
     private KeyValue keyValueTop;
@@ -35,32 +46,26 @@ public class MenuItemController {
 
     private FadeTransition fadeInRemove;
     private FadeTransition fadeOutRemove;
-
+    private double itemPrice = 0.00;
 
 
     @FXML
-    public void initialize(String itemName, String category, String itemDescription, List<String> itemIngredients, Double itemPrice) {
+    public void initialize(String itemName, String category, String itemDescription, List<String> itemIngredients, Double priceItem) {
         name.setText(itemName);
         description.setText(itemDescription);
         ingredients.setText(String.join(", ", itemIngredients));
-        price.setText(String.format("%.2f", itemPrice));
+        itemPrice = priceItem;
+        price.setText(String.format("%.2f", itemPrice) + " €");
         itemView.setMaxHeight(60);
+        addIcon.setIconColor(Color.WHITE);
+        removeIcon.setIconColor(Color.WHITE);
+        add.setGraphic(addIcon);
+        remove.setGraphic(removeIcon);
+        doAnimation();
+        //itemView.getChildren().add(createCustomItem());
+        itemView.setOnMouseClicked(event -> {
 
-        // Animazioni
-        fadeInRemove = new FadeTransition(Duration.millis(250), remove);
-        fadeInRemove.setFromValue(0);
-        fadeInRemove.setToValue(1.0);
-        fadeOutRemove = new FadeTransition(Duration.millis(250), remove);
-        fadeOutRemove.setFromValue(1.0);
-        fadeOutRemove.setToValue(0);
-        keyValueCenter = new KeyValue(add.translateYProperty(), 25);
-        keyValueTop = new KeyValue(add.translateYProperty(), 0);
-        // Sposta add al centro all'inizio
-        KeyFrame keyFrameCenter = new KeyFrame(Duration.millis(250), keyValueCenter);
-        KeyFrame keyFrameTop = new KeyFrame(Duration.millis(250), keyValueTop);
-        timelineCenter = new Timeline(keyFrameCenter);
-        timelineTop = new Timeline(keyFrameTop);
-        timelineCenter.play();
+        });
     }
 
     @FXML
@@ -81,13 +86,45 @@ public class MenuItemController {
         if (itemQuantity > 0) {
             itemQuantity--;
         }
-        quantity.setText(String.valueOf(itemQuantity) + "x");
+
         if (itemQuantity == 0) {
             timelineCenter.play();
             itemView.getStyleClass().remove("addedItem");
             fadeOutRemove.play();
             quantity.setVisible(false);
             remove.setVisible(false);
+        } else {
+            quantity.setText(String.valueOf(itemQuantity) + "x");
         }
+    }
+
+    private void doAnimation() {
+        fadeInRemove = new FadeTransition(Duration.millis(250), remove);
+        fadeInRemove.setFromValue(0);
+        fadeInRemove.setToValue(1.0);
+        fadeOutRemove = new FadeTransition(Duration.millis(250), remove);
+        fadeOutRemove.setFromValue(1.0);
+        fadeOutRemove.setToValue(0);
+        keyValueCenter = new KeyValue(add.translateYProperty(), 25);
+        keyValueTop = new KeyValue(add.translateYProperty(), 0);
+        // Sposta add al centro all'inizio
+        KeyFrame keyFrameCenter = new KeyFrame(Duration.millis(250), keyValueCenter);
+        KeyFrame keyFrameTop = new KeyFrame(Duration.millis(250), keyValueTop);
+        timelineCenter = new Timeline(keyFrameCenter);
+        timelineTop = new Timeline(keyFrameTop);
+        timelineCenter.play();
+    }
+
+    private HBox createCustomItem() {
+        HBox hBox = new HBox();
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(Settings.SCENE_PATH + "view/custom-item.fxml"));
+            hBox = fxmlLoader.load();
+            CustomItemController customItemController = fxmlLoader.getController();
+            //customItemController.initialize();
+        } catch (IOException ignored) {
+
+        }
+        return hBox;
     }
 }
