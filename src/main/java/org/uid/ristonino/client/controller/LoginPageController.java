8094a958b6bac29f959685b8aa2ab5f076823776
Shadw <34161328+Shadowz-git@users.noreply.github.com>
2024-05-22 16:23:56 +0200

    Aggiornata l'interfaccia per il login

--- src/main/java/org/uid/ristonino/client/controller/LoginPageController.java
+++ src/main/java/org/uid/ristonino/client/controller/LoginPageController.java
@@ -3,32 +3,56 @@ package org.uid.ristonino.client.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import org.kordamp.ikonli.javafx.FontIcon;
 import org.uid.ristonino.client.view.SceneHandler;
 
 public class LoginPageController {
-    public boolean isDarkMode = true;
-    @FXML
-    private Button Pulsante;
+    @FXML Label numCoperti;
+    @FXML Button buttonRemCop;
+    @FXML Button buttonAddCop;
+
+    private int maxNumeroCoperti = 12;
+
+    private int numeroCoperti = 1;
+
+    private FontIcon fontIconRemove = new FontIcon("mdrmz-person_remove_alt_1");
+    private FontIcon fontIconAdd = new FontIcon("mdrmz-person_add_alt_1");
+    private FontIcon people = new FontIcon("mdsmz-people");
 
     @FXML
-    private Label prova;
+    private void initialize() {
+        fontIconRemove.setIconSize(30);
+        fontIconAdd.setIconSize(30);
+        people.setIconSize(30);
+        buttonRemCop.setGraphic(fontIconRemove);
+        buttonAddCop.setGraphic(fontIconAdd);
+        numCoperti.setGraphic(people);
+    }
 
     @FXML
-    private void cambiaTesto() {
-        if (isDarkMode) {
-            SceneHandler.getInstance().changeTheme("light");
-            prova.setText("TEST RIUSCITO - Changed theme to light");
-            this.isDarkMode = false;
-        } else {
-            SceneHandler.getInstance().changeTheme("dark");
-            prova.setText("TEST RIUSCITO - Changed theme to dark");
-            this.isDarkMode = true;
+    private void addCoperto() {
+        if (numeroCoperti < maxNumeroCoperti) {
+            numeroCoperti++;
+            numCoperti.setText(String.valueOf(numeroCoperti));
+            buttonRemCop.setDisable(false);
+        }
+
+    }
+
+    @FXML
+    private void removeCoperto() {
+        if (numeroCoperti > 1) {
+            numeroCoperti--;
+            numCoperti.setText(String.valueOf(numeroCoperti));
+            if (numeroCoperti == 1) {
+                buttonRemCop.setDisable(true);
+            }
         }
     }
 
     @FXML
     private void vaiHome() {
-        SceneHandler.getInstance().changeTheme("dark");
+        SceneHandler.getInstance().changeTheme("light");
         SceneHandler.getInstance().createHomeScene();
     }
 }
