3d7cb3c05d02c45c59a94e1737366d0c088e6cbf
Shadw <34161328+Shadowz-git@users.noreply.github.com>
2024-05-22 16:19:37 +0200

    Gestore API

--- src/main/java/org/uid/ristonino/client/model/api/ApiHandler.java
+++ src/main/java/org/uid/ristonino/client/model/api/ApiHandler.java
@@ -1,51 +1,53 @@
 package org.uid.ristonino.client.model.api;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.io.DataOutputStream;
-import java.nio.charset.StandardCharsets;
+import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.json.JsonObject;
+import org.uid.ristonino.client.model.Debug;
+import org.uid.ristonino.client.model.types.Item;
 
 
 public class ApiHandler {
-    public void sendOrder(JSONObject order) {
+    private final Vertx vertx = Vertx.vertx();
+    private final HttpClient client = vertx.createHttpClient();
+    public static ApiHandler INSTANCE = new ApiHandler();
+
+    private ApiHandler() {
         try {
-            // Definisci l'URL del servizio che vuoi chiamare
-            URL url = new URL("http://example.com/api/resource");
+            client.request(HttpMethod.GET, 8080, "localhost", "/")
+                    .compose(req -> req.send()
+                            .compose(resp -> resp.body()
+                                    .onSuccess(body -> System.out.println("GET Response: " + body.toString()))));
+                                    //.onFailure(Throwable::printStackTrace)))
+                    //.onFailure(Throwable::printStackTrace);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-            // Apre una connessione HTTP
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("POST");
-            conn.setRequestProperty("Content-Type", "application/json");
-            conn.setDoOutput(true);
+    public static ApiHandler getInstance() {
+        return INSTANCE;
+    }
 
-            // Crea un oggetto JSON con i dati da inviare
-            JSONObject jsonInput = new JSONObject();
-            jsonInput.put("param1", "value1");
-            jsonInput.put("param2", "value2");
-
-            // Scrivi i dati JSON nel corpo della richiesta
-            try (DataOutputStream wr = new DataOutputStream(conn.getOutputStream())) {
-                byte[] postData = jsonInput.toString().getBytes();
-                wr.write(postData);
-            }
-
-            // Leggi la risposta
-            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            StringBuilder response = new StringBuilder();
-            String line;
-            while ((line = rd.readLine()) != null) {
-                response.append(line);
-            }
-            rd.close();
-
-            // Stampa la risposta
-            System.out.println(response.toString());
+    public void sendOrder(JsonObject order) {
+        try {
+            client.request(HttpMethod.POST, 8080, "localhost", "/")
+                    .compose(req -> req.putHeader("content-type", "application/json")
+                            .send(Buffer.buffer(order.encode()))
+                            .compose(resp -> resp.body()
+                                    .onSuccess(body -> {
+                                        System.out.println("POST Response: " + body.toString());
+                                    })
+                                    .onFailure(Throwable::printStackTrace)))
+                    .onFailure(Throwable::printStackTrace);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void closeConnection() {
+        vertx.close();
     }
 }
