1b7bc34a0a281c0b6511fd465b0c80ccbd7ebe7d
Shadw <34161328+Shadowz-git@users.noreply.github.com>
2024-05-22 16:24:40 +0200

    Aggiornate dipendenze per ikonli e Vertx

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,11 @@
 module org.uid.ristonino.client {
+    requires javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
 
     requires org.kordamp.ikonli.javafx;
     requires org.json;
+    requires io.vertx.core;
 
     opens org.uid.ristonino.client to javafx.fxml;
     opens org.uid.ristonino.client.controller to javafx.fxml;
