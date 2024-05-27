f9f1dafc86921786ee45bb9e3e6412c85c30cd4d
Shadw <34161328+Shadowz-git@users.noreply.github.com>
2024-05-27 23:17:34 +0200

    Evento per notificare l'aggiunta di un ordine nella lista degli ordini

--- src/main/java/org/uid/ristonino/client/model/events/AddOrder.java
+++ src/main/java/org/uid/ristonino/client/model/events/AddOrder.java
@@ -2,22 +2,27 @@ package org.uid.ristonino.client.model.events;
 
 import javafx.event.Event;
 import javafx.event.EventType;
+import org.uid.ristonino.client.model.types.Order;
 
 public class AddOrder extends Event {
     public static final EventType<AddOrder> ORDER_ADDED = new EventType<>(Event.ANY, "ADD_ORDER_LIST");
 
-    private final String orderName;
-    private final String ingredients;
+    private final String itemId;
+    private Order order;
 
-    public AddOrder(String orderName, String ingredients) {
+    public AddOrder(String itemId, Order order) {
         super(ORDER_ADDED);
-        this.orderName = orderName;
-        this.ingredients = ingredients;
+        this.itemId = itemId;
+        this.order = order;
     }
-    public String getOrderName() {
-        return orderName;
+    public void changeOrder(Order order) {
+        this.order = order;
     }
-    public String getIngredients() {
-        return ingredients;
+
+    public String getItemId() {
+        return itemId;
+    }
+    public Order getOrder() {
+        return order;
     }
 }
