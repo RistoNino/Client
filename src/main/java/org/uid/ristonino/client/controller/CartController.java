0eeb9fc0fd316f4aea4dc0735809494ee14f87ab
Shadw <34161328+Shadowz-git@users.noreply.github.com>
2024-05-27 23:08:41 +0200

    Interfaccia Cart e controller per il cambio prezzo

--- src/main/java/org/uid/ristonino/client/controller/CartController.java
+++ src/main/java/org/uid/ristonino/client/controller/CartController.java
@@ -1,5 +1,19 @@
 package org.uid.ristonino.client.controller;
 
-public class CartController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import org.uid.ristonino.client.model.events.EventBus;
+import org.uid.ristonino.client.model.events.UpdateCart;
 
+public class CartController {
+    @FXML
+    private Label totalPrice;
+
+    @FXML
+    public void initialize() {
+        EventBus.getInstance().addEventHandler(UpdateCart.EVENT_TYPE,event -> {
+            double total = ((UpdateCart) event).getTotal();
+            totalPrice.setText(String.format("%.2f", total));
+        });
+    }
 }
