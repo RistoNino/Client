26c67423ebc608935c419d1f16cc42cdbf38ba37
Shadw <34161328+Shadowz-git@users.noreply.github.com>
2024-05-27 23:12:58 +0200

    Aggiornati metodi Order

--- src/main/java/org/uid/ristonino/client/model/types/Order.java
+++ src/main/java/org/uid/ristonino/client/model/types/Order.java
@@ -3,18 +3,18 @@ package org.uid.ristonino.client.model.types;
 import java.util.List;
 
 public class Order {
-    private int tableId;
     private String itemName;
+    private double price;
     private String removedIngredients;
-    private String addedIngredients;
+    private String notes;
     private int quantity;
 
-    public Order(int tableId, String itemName, int quantity, List<String> removedIngredients, List<String> addedIngredients) {
-        this.tableId = tableId;
+    public Order(String itemName, int quantity, double price, List<String> removedIngredients, String notes) {
         this.itemName = itemName;
         this.quantity = quantity;
+        this.price = price;
         this.removedIngredients = createIngString(removedIngredients);
-        this.addedIngredients = createIngString(addedIngredients);
+        this.notes = notes;
     }
 
     private String createIngString(List<String> ingredients) {
@@ -27,5 +27,36 @@ public class Order {
         }
         return stringBuilder.toString();
     }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 
+    public String getItemName() {
+        return itemName;
+    }
+
+    public String getRemovedIngredients() {
+        if (!removedIngredients.isEmpty()) {
+            return "NO - " + removedIngredients;
+        }
+        return removedIngredients;
+    }
+    public String getNotes() {
+        return notes;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setRemovedIngredients(List<String> removedIngredients) {
+        this.removedIngredients = createIngString(removedIngredients);
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
 }
