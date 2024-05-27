06c5ced8c4ed0ededabd385090b2b8e4461d2601
Shadw <34161328+Shadowz-git@users.noreply.github.com>
2024-05-27 23:10:33 +0200

    Evento per l'aggiornamento del Cart

---
+++ src/main/java/org/uid/ristonino/client/model/events/UpdateCart.java
@@ -0,0 +1,19 @@
+package org.uid.ristonino.client.model.events;
+
+import javafx.event.Event;
+import javafx.event.EventType;
+import org.uid.ristonino.client.model.types.Cart;
+
+public class UpdateCart extends Event {
+    public static final EventType<UpdateCart> EVENT_TYPE = new EventType<>(Event.ANY, "UPDATED_CART");
+
+    private Cart cart;
+
+    public UpdateCart(Cart cart) {
+        super(EVENT_TYPE);
+        this.cart = cart;
+    }
+    public double getTotal() {
+        return cart.getTotal();
+    }
+}
