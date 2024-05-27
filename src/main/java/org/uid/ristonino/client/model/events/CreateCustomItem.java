7d61432c5c7fbc2f4d51da34983b3442bf1f1f89
Shadw <34161328+Shadowz-git@users.noreply.github.com>
2024-05-27 23:09:47 +0200

    Creazione dell'Item custom (Modal)

---
+++ src/main/java/org/uid/ristonino/client/model/events/CreateCustomItem.java
@@ -0,0 +1,36 @@
+package org.uid.ristonino.client.model.events;
+
+import javafx.event.Event;
+import javafx.event.EventType;
+
+import java.util.List;
+
+public class CreateCustomItem extends Event {
+    public static final EventType<CreateCustomItem> EVENT_TYPE = new EventType<>(Event.ANY, "CREATE_CUSTOM_ITEM");
+
+    private final String name;
+    private final String description;
+    private final List<String> ingredients;
+    private final double price;
+
+
+    public CreateCustomItem(String name, String description, List<String> ingredients, double price) {
+        super(EVENT_TYPE);
+        this.name = name;
+        this.description = description;
+        this.ingredients = ingredients;
+        this.price = price;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+    public double getPrice() {
+        return price;
+    }
+}
