494289dfbebba38221a56a819cc1086492133740
Shadw <34161328+Shadowz-git@users.noreply.github.com>
2024-06-06 19:19:28 +0200

    Aggiunto Evento per aggiornare gli ordini

--- src/main/java/org/uid/ristonino/client/model/events/UpdateOrders.java
+++ src/main/java/org/uid/ristonino/client/model/events/UpdateOrders.java
@@ -3,7 +3,6 @@ package org.uid.ristonino.client.model.events;
 import javafx.event.Event;
 import javafx.event.EventType;
 
-// TODO: Triggera in Item il reset di tutte le quantità e cambia lo stato agli Order nelle OrderList
 public class UpdateOrders extends Event {
     public static final EventType<UpdateOrders> EVENT_TYPE = new EventType<>(Event.ANY, "UPDATE_ORDERS");
 
