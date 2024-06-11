3c8c5b0564717a4f69002bffd20e72263667c089
Shadw <34161328+Shadowz-git@users.noreply.github.com>
2024-06-11 16:49:56 +0200

    Aggiornamenti vari

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,7 @@ module org.uid.ristonino.client {
     requires org.kordamp.ikonli.javafx;
     requires org.json;
     requires io.vertx.core;
+    requires io.github.cdimascio.dotenv.java;
 
     opens org.uid.ristonino.client to javafx.fxml;
     opens org.uid.ristonino.client.controller to javafx.fxml;
