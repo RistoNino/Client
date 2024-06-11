9d25df9eceec50b3dd161932ff88e6e3a43c73ff
Shadw <34161328+Shadowz-git@users.noreply.github.com>
2024-06-11 16:48:58 +0200

    Rimossa funzione inutilizzata

--- src/main/java/org/uid/ristonino/client/model/Debug.java
+++ src/main/java/org/uid/ristonino/client/model/Debug.java
@@ -64,16 +64,9 @@ public class Debug {
 
     public final static List<Item> items = new ArrayList<>(List.of(item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12, item13, item14, item15, item16, item17, item18, item19, item20, item21, item22, item23, item24, item25, item26, item27, item28, item29, item30));
 
-
     public static void print(String args) {
         if (IS_ACTIVE) {
             System.out.println(args);
         }
-    }
-
-    public static void sendOrder() {
-        JsonObject json = new JsonObject();
-        json.put("items", items.get(0));
-        ApiHandler.getInstance().sendOrder(json);
     }
 }
