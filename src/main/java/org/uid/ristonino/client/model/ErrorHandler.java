cf76ebd7b0dbdf5bfcb04e617bb9688583b10566
Shadw <34161328+Shadowz-git@users.noreply.github.com>
2024-07-08 15:33:29 +0200

    Aggiunto prototipo per classi errore

---
+++ src/main/java/org/uid/ristonino/client/model/ErrorHandler.java
@@ -0,0 +1,5 @@
+package org.uid.ristonino.client.model;
+
+public class ErrorHandler {
+
+}
--- src/main/java/org/uid/ristonino/client/model/ErrorMessage.java
+++
@@ -1,8 +0,0 @@
-package org.uid.ristonino.client.model;
-
-public class ErrorMessage {
-    // Ordini
-    private final static String orderError = "E' avvenuto un errore durante la richiesta dell'ordine, riprovare.";
-    // Comunicazione Server
-    private final static String connectionFailed = "Errore! Connessione al server interrotta.";
-}
